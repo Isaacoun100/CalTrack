e4b4340b73e0b4d35c69dc3722900b93ead88335
Isaac Herrera Monge <51173904+Isaacoun100@users.noreply.github.com>
2026-01-27 03:10:27 -0600

    User sign in created!
    The clients will now be able to put their credentials and be redirected to user or nutritionist, also they'll will have the option to create a new user

--- CalTrack/src/app/home/home.ts
+++ CalTrack/src/app/home/home.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrls: ['./home.css']
 })
 export class Home {
 
+  username = '';
+  password = '';
+  errorMessage = '';
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) {}
+
+  login() {
+    this.http.get<any>('clients.json').subscribe(data => {
+
+      const nutri = data.nutritionist.find(
+        (n: any) => n.username === this.username && n.password === this.password
+      );
+
+      if (nutri) {
+        this.router.navigate(['/nutri-dashboard']);
+        return;
+      }
+
+      const user = data.users.find(
+        (u: any) => u.username === this.username && u.password === this.password
+      );
+
+      if (user) {
+        this.router.navigate(['/user-dashboard']);
+        return;
+      }
+
+      this.errorMessage = 'Invalid username or password';
+    });
+  }
+
+  goToSignUp() {
+    this.router.navigate(['/sign-up']);
+  }
 }
