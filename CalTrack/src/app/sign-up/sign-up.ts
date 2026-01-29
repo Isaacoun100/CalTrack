1563c4b7b3593799fc3807848ebe68e3bf9424ce
Isaac Herrera Monge <51173904+Isaacoun100@users.noreply.github.com>
2026-01-29 04:07:54 -0600

    Sign up added
    The sign up now works perfectly, but more than than it also makes requests and creates users on the database

--- CalTrack/src/app/sign-up/sign-up.ts
+++ CalTrack/src/app/sign-up/sign-up.ts
@@ -1,11 +1,64 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { supabase } from '../supabase';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './sign-up.html',
-  styleUrl: './sign-up.css',
+  styleUrls: ['./sign-up.css']
 })
-export class SignUp {
+export class SignUpComponent {
 
+  showPassword = false;
+  errorMessage = '';
+
+  user = {
+    firstName: '',
+    firstLastName: '',
+    secondLastName: '',
+    height: null,
+    weight: null,
+    age: null,
+    phoneNumber: '',
+    username: '',
+    password: ''
+  };
+
+  constructor(private router: Router) {}
+
+  navigateToLogin(event: Event) {
+    event.preventDefault();
+    this.router.navigate(['']);
+  }
+
+  async createUser(formData: any) {
+    const { error } = await supabase
+      .from('users')
+      .insert([{
+        firstName: formData.firstName,
+        firstLastName: formData.firstLastName,
+        secondLastName: formData.secondLastName,
+        height: formData.height,
+        weight: formData.weight,
+        age: formData.age,
+        phoneNumber: formData.phoneNumber,
+        username: formData.username,
+        password: formData.password
+      }]);
+
+    if (error) {
+      if (error.code === '23505') {
+        this.errorMessage = 'Email already exists';
+      } else {
+        this.errorMessage = 'Something went wrong';
+        console.error(error);
+      }
+    } else {
+      this.router.navigate(['']);
+    }
+  }
 }
