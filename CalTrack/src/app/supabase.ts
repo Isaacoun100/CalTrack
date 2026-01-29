48975edeac0b6594464507d87795e1574818d084
Isaac Herrera Monge <51173904+Isaacoun100@users.noreply.github.com>
2026-01-29 04:06:03 -0600

    Support for supabase
    We will no longer use JSON for the database, now well use supabase as a simple DB manager and host

---
+++ CalTrack/src/app/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+  'https://kmbhzpfwhzxpobhjbyvt.supabase.co',
+  'sb_publishable_hu-YJAvolnoKaMh2dvPC1w_Ou_W19Av'
+);
