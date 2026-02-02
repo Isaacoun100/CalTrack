da1605370118568682d3b90728946da14ae0abb2
Isaac Herrera Monge <51173904+Isaacoun100@users.noreply.github.com>
2026-02-02 13:49:29 -0600

    App route corrected

--- CalTrack/src/app/app.routes.ts
+++ CalTrack/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { UserDashboardComponent } from './user-dashboard/user-dashboard';
 import { CheckGoals } from './check-goals/check-goals';
 import { CheckUsers } from './check-users/check-users';
 import { LogCalComponent } from './log-cal/log-cal';
-import { NutriDashboard } from './nutri-dashboard/nutri-dashboard';
+import { NutriDashboardComponent } from './nutri-dashboard/nutri-dashboard';
 import { SignUpComponent } from './sign-up/sign-up';
 import { GestionAlimentos } from './gestion-alimentos/gestion-alimentos';
 import { VerUsuarios } from './ver-usuarios/ver-usuarios';
@@ -12,7 +12,7 @@ import { VerUsuarios } from './ver-usuarios/ver-usuarios';
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'user-dashboard', component: UserDashboardComponent },
-  { path: 'nutri-dashboard', component: NutriDashboard },
+  { path: 'nutri-dashboard', component: NutriDashboardComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'check-goals', component:CheckGoals},
   { path: 'check-users', component:CheckUsers},
