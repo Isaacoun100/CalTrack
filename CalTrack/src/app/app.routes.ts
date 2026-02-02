7d6b50729b39ff221e0805f4904b90f6e44a25ba
Isaac Herrera Monge <51173904+Isaacoun100@users.noreply.github.com>
2026-02-02 13:28:46 -0600

    Calorie log creaed
    The users will now be able to log their intake into the website

--- CalTrack/src/app/app.routes.ts
+++ CalTrack/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Home } from './home/home';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard';
 import { CheckGoals } from './check-goals/check-goals';
 import { CheckUsers } from './check-users/check-users';
-import { LogCal } from './log-cal/log-cal';
+import { LogCalComponent } from './log-cal/log-cal';
 import { NutriDashboard } from './nutri-dashboard/nutri-dashboard';
 import { SignUpComponent } from './sign-up/sign-up';
 
@@ -14,5 +14,5 @@ export const routes: Routes = [
   { path: 'sign-up', component: SignUpComponent },
   { path: 'check-goals', component:CheckGoals},
   { path: 'check-users', component:CheckUsers},
-  { path: 'log-cal', component:LogCal}
+  { path: 'log-cal', component:LogCalComponent}
 ];
